b44287b79504bc63aef6c37a9cf1e3dd3787c6f7
Kavinda Liyanaarachchi <110520736+Kavinda-liyan@users.noreply.github.com>
2023-01-04 01:52:07 +0530

    Add files via upload

---
+++ assets/php/navbar.php
@@ -0,0 +1,12 @@
+<nav class="navbar navbar-light navbar-expand-md py-2 shadow-sm ">
+        <div class="container"><img src="assets/img/Navbar%20logo.png" width="51" height="51"><a class="navbar-brand d-flex align-items-center" href="#"><span style="font-family: 'Noto Sans Sinhala', sans-serif;font-weight: bold;">&nbsp;<span style="color:#0071bc; font-weight: bolder; font-size: 30px;">සණස</span> ගොඩිගමුව ශාඛාව</span></a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-2"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navcol-2">
+                <ul class="navbar-nav ms-auto font-weight-bold">
+                    <li class="nav-item"><a class="nav-link " href="#">මුල් පිටුව</a></li>
+                    <li class="nav-item"><a class="nav-link" href="../../history.php">ඉතිහාසය</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">ගිණුම්</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">ප්‍රජා සේවාවන්&nbsp;</a></li>
+                <!-- </ul><a class="btn btn-primary ms-md-2" role="button" href="#" style="background: linear-gradient(#0072bc, #013b60), var(--bs-blue);border-color: var(--bs-border-color-translucent);font-size: 20px;border-radius: 12px;border-top-left-radius: 57;">පිවිසීම</a> -->
+            </div>
+        </div>
+</nav>
