e2c54d1aa5a54ef53cd5fa8fa86f65a827f07da9
Kavinda Liyanaarachchi <110520736+Kavinda-liyan@users.noreply.github.com>
2023-01-04 01:51:10 +0530

    Add files via upload

---
+++ history.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Godigamuwa Sanasa</title>
+    <link rel="icon" type="image/x-icon" href="assets/img/Navbar logo.png">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Noto+Sans+Sinhala&amp;display=swap">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/6.4.8/swiper-bundle.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Kanit:ital,wght@0,600;1,500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/Navbar-Right-Links-icons.css">
+    <link rel="stylesheet" href="assets/css/Simple-Slider-Simple-Slider.css">
+    <link rel="stylesheet" href="assets/css/history.css">
+</head>
+<body>
+    <!-- NAVBAR -->
+    <?php include('assets/php/navbar.php') ?>
+    <!-- NAVBAR END -->
+    
+    <section>
+        <div class="herosection text-center">
+<h3 style="font-family: 'Bebas Neue', cursive;font-family: 'Kanit', sans-serif;font-weight: bold; " class="p-2">ඉතිහාසය හා වර්තමානය</h3>
+        </div>
+        <hr style="height:2px;background-color:#0071bc" class="p-1">
+    </section>
+</body>
+</html>
