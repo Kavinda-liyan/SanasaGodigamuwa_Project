d1bb342d3f0d818c55efaf5ffae1f150e7ccd770
Kavinda Liyanaarachchi <110520736+Kavinda-liyan@users.noreply.github.com>
2023-01-04 13:33:26 +0530

    Add files via upload

---
+++ accounts.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Godigamuwa Sanasa</title>
+    <link rel="icon" type="image/x-icon" href="assets/img/Navbar logo.png">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+    
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Noto+Sans+Sinhala&amp;display=swap">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/6.4.8/swiper-bundle.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Kanit:ital,wght@0,600;1,500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/Navbar-Right-Links-icons.css">
+    <link rel="stylesheet" href="assets/css/Simple-Slider-Simple-Slider.css">
+    <link rel="stylesheet" href="assets/css/accounts.css">
+    <link rel="stylesheet" href="assets/css/navbar.css">
+</head>
+<body>
+    <!-- NAVBAR -->
+    <?php include('assets/php/navbar.php') ?>
+    <!-- NAVBAR END -->
+    <section>
+        <div class="herosection text-center">
+<h3 style="font-family: 'Bebas Neue', cursive;font-family: 'Kanit', sans-serif;font-weight: bold; " class="p-2 my-1">ගිණුම් </h3>
+        </div>
+        <hr style="height:2px;background-color:#0071bc" class="p-1">
+        <div class="container">
+            <div class="accounts">
+                <div class="acctype">
+                    <h3 class="m-3 p-3">ඉතුරුම් ගිණුම්.</h3>
+                </div>
+                <div class="accountlist mx-5 px-5">
+                    <p>සාමාජික මෙන්ම සාමාජික නොවන පිරිස ගේ ඉතුරුම් වෙනුවෙන් තරඟකාරී මූල්‍ය ආයතන 
+                සමග සසඳමින් ඉහළ පොලියක් මෙන්ම උපරිම මූල්‍යම සුරක්ෂිතතාව සැලසීම අපගේ ප්‍රධාන අරමුණකි .</p>
+                    <div class="container px-5 mx-5 py-0">
+                    <p>සාමාජික ඉතුරුම්.</p>
+                    <p>සාමාජික නොවන ඉතුරුම්.</p>
+                    <p>සණස සුපිරි ඉතුරුම්.</p>
+                    <p>ආරෝග්‍යා ඉතුරුම්.</p>
+                    <p>විදේශ චාරිකා ඉතුරුම්.</p>
+
+                    </div>
+                    <hr>
+                    <p>
+                        ළමා ඉතුරුම් ගිණුම්
+
+                    </p>
+                    <div class="container px-5 mx-5 py-0">
+                    <p>
+                        ස්වේච්ඡා ළමා ඉතුරුම්.
+
+                    </p>
+                    </div>
+                
+                </div>
+            </div>
+
+            <div class="accounts">
+                <div class="acctype">
+                    <h3 class="m-3 p-3">ස්ථාවර තැන්පත්.</h3>
+                </div>
+                <div class="accountlist mx-5 px-5">
+                    <div class="container px-5 mx-5 py-0">
+                    <p>වාර්ෂික ස්ථාවර තැන්පත්.</p>
+                    <p>ත්‍රෛයිමාසික ස්ථාවර තැන්පත්. </p>
+                    <p>වැඩිහිටි සත්කාර ස්ථාවර තැන්පත්.</p>
+
+                    </div>
+                 
+                
+                </div>
+            </div>
+
+            <div class="accounts">
+                <div class="acctype">
+                    <h3 class="m-3 p-3">ණය වර්ග.</h3>
+                </div>
+                <div class="accountlist mx-5 px-5">
+                    <div class="container px-5 mx-5 py-0">
+                    <p>ව්‍යාපාර පුද්ගල ණය.</p>
+                    <p>ව්‍යාපාර දේපල ණය. </p>
+                    <p>නිවාස පුද්ගල ණය.</p>
+                    <p>නිවාස දේපල ණය.</p>
+                    <p>අතමාරු ණය. </p>
+                    <p>අධ්‍යාපන ණය.</p>
+                    <p>ක්ෂණික ණය.</p>
+                    <p>අවුරුදු ණය. </p>
+                    <p>ස්වයං රැකියා ණය.</p>
+                    <br>
+                    <p><span><i class="fa fa-stop px-1" aria-hidden="true"></i></span>පුද්ගල ණය සඳහා පුද්ගල ඇප මත රු.500,000/= ක් දක්වා ලබාගත හැක.</p>
+                    <p><span><i class="fa fa-stop px-1" aria-hidden="true"></i></span>දේපල ණය සඳහා දේපල ඇප මත රු.7,500,000/= ක් දක්වා ලබාගත හැක.</p>
+                    <p><span><i class="fa fa-stop px-1" aria-hidden="true"></i></span>අතමාරු ණය පුද්ගල ඇප මත රු.150,000/= ක් දක්වා ලබාගත හැක.</p>
+                    <p><span><i class="fa fa-stop px-1" aria-hidden="true"></i></span>ස්වයං රැකියා ණය පුද්ගල ඇප මත රු.250,000/= ක් දක්වා ලබාගත හැක.</p>
+                    
+
+                    </div>
+                 
+                
+                </div>
+            </div>
+
+            <div class="accounts">
+                <div class="acctype">
+                    <h3 class="m-3 p-3">උකස් ණය.</h3>
+                </div>
+                <div class="accountlist mx-5 px-5">
+                   <p>වසරක ගිවිසුම් කාලයකට රන් භාණ්ඩ සඳහා රුපියල් 60,000/= ක අත්තිකාරම් මුදලක්ද,
+                    මාස හයක කාල සීමාවක් සඳහා රුපියල් 75,000/= ක අත්තිකාරම් මුදලක්ද ලබා දෙනු ලැබේ.
+                    මාසිකව ණයක් ලෙස මුදල් අඩු කළ හැක, වෙනත් මූල්‍ය ආයතන වලට වඩා සහනදායී පොලී අනුපාත ඇත .
+                   </p>
+                 
+                
+                </div>
+            </div>
+            <div class="accounts">
+                <div class="acctype">
+                    <h3 class="m-3 p-3">ස්ථාවර තැන්පත් අත්තිකාරම් ණය.</h3>
+                </div>
+                <div class="accountlist mx-5 px-5">
+                   <p>ස්ථාවර තැන්පතුව ඇපයට තබා මාසිකව පොළිය ලබාගන්නා අය සඳහා තැන්පතු වටිනාකමින් 85% ක්ද 
+                    වාර්ෂිකව පොලිය ලබාගන්නා අය සඳහා 90% ක් මුදලක්ද ණයක් ලෙස ලබාගත හැක .
+                   </p>
+                 
+                
+                </div>
+            </div>
+
+
+        </div>
+    </section>
+     <!-- FOOTER -->
+     <?php include('assets/php/footer.php') ?>
+
+<!-- END FOOTER -->
+    
+</body>
+</html>
